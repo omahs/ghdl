ab96b68044564d2557043640270af9913e2e4e53
Tristan Gingold <tgingold@free.fr>
2024-07-02 20:56:37 +0200

    gha: static link with llvm on macos

--- scripts/ci-run.sh
+++ scripts/ci-run.sh
@@ -263,7 +263,7 @@ build () {
 
   if [ "x$IS_MACOS" = "xtrue" -a "$GITHUB_OS_VER" -ge 13 ]; then
       # Use classic ld and not lld (which simply crashes)
-      CONFIG_OPTS+=" LDFLAGS=-Wl,-ld_classic"
+      CONFIG_OPTS+=" LDFLAGS=-Wl,-ld_classic LLVM_CONFIG_FLAGS=--link-static"
   fi
 
   if [ ! "`echo $BACK | grep gcc`" ]; then
